284671b0324e19584ab6bd2e237531e76c31ca93
denadan <denadan.sa@gmail.com>
2021-03-05 06:55:27 +0300

    ReplaceValidateDrop

--- source/CustomSlotInfo.cs
+++ source/CustomSlotInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using BattleTech;
@@ -41,7 +42,6 @@ namespace CustomSlots
 
         public virtual void OnInstalled(WorkOrderEntry_InstallComponent order, SimGameState state, MechDef mech)
         {
-
             var inventory = mech.Inventory.ToList();
             CustomSlotControler.AdjustMechDefaults(mech, state, SlotName, inventory);
             mech.SetInventory(inventory.ToArray());
@@ -56,64 +56,37 @@ namespace CustomSlots
         public string ReplaceValidateDrop(MechLabItemSlotElement drop_item, LocationHelper location, List<IChange> changes)
         {
             var mech = location.mechLab.activeMechDef;
-//            var total = CustomSlotControler.SlotsTotal(mech);
-            var sdef = CustomSlotControler.GetDefInfo(mech);
-
-            //var used_fixed = mech.Inventory.Where(i => i.IsModuleFixed(mech)).Where(i => i.Is<CustomSlotInfo>())
-            //    .Sum(i => i.GetComponent<CustomSlotInfo>().SpecSlotUsed);
-
-            var installed = mech.Inventory
-                .Where(i => i.Is<CustomSlotInfo>() && !i.IsModuleFixed(mech))
-                .Select(i => new { item = i, spinfo = i.GetComponent<CustomSlotInfo>()} ).ToList();
-
-            var defaults = installed.Where(i => i.item.IsDefault()).ToList();
-            var notdefaults = installed.Where(i => !i.item.IsDefault()).ToList();
-
-            var used = defaults.Sum(i => i.spinfo.SpecSlotUsed);
+            var sdef = SlotsInfoDatabase.GetMechInfoByType(mech, SlotName);
+            var mountlocation = location.widget.loadout.Location;
             
-            //remove additional non defaults if need
-            if (SpecSlotUsed > used)
-            {
-                
-                foreach (var item_to_replace in notdefaults)
+            if(changes.Count > 1)
+                return String.Empty;
+
+            var total = sdef[mountlocation]?.SlotCount ?? 0;
+            if (total == 0)
+                return string.Format(Control.Instance.Settings.ErrorMechLab_Slots,
+                    drop_item.ComponentRef.Def.Description.Name, location.LocationName);
+
+
+            var replacable = mech.Inventory
+                .Where(i => i.MountedLocation == mountlocation)
+                .Select(i => new {item = i, slot = i.GetComponent<IUseSlots>()})
+                .Where(i => i.slot != null && i.slot.SlotName == SlotName)
+                .Select(i => new
                 {
-                    used += item_to_replace.spinfo.SpecSlotUsed;
+                    item = i.item,
+                    slot = i.slot,
+                    used_slot = i.slot.GetSlotsUsed(mech),
+                    used_sup = sdef.Descriptor.HaveSupports ? i.slot.GetSupportUsed(mech) : 0
+                })
+                .ToList();
+            var max_support = sdef.Descriptor.HaveSupports ? CustomSlotControler.GetSupportsForLocation(mech, SlotName, mountlocation) : 0;
 
-                    var slot_item = location.LocalInventory.FirstOrDefault(i => i.ComponentRef == item_to_replace.item);
-                    changes.Add(new RemoveChange(ChassisLocations.CenterTorso, slot_item));
-                    if(SpecSlotUsed <= used + used)
-                        break;
-                }
-            }
+            var used_slots = replacable.Sum(i => i.used_slot);
+            var used_sups = sdef.Descriptor.HaveSupports ? replacable.Sum(i => i.used_sup) : 0;
+            var slot_need = GetSlotsUsed(mech);
+            var supp_need = GetSupportUsed(mech);
 
-            //remove defaults
-            foreach (var def_item in defaults)
-            {
-                var slot_item = location.LocalInventory.FirstOrDefault(i => i.ComponentRef == def_item.item);
-                changes.Add(new RemoveChange(ChassisLocations.CenterTorso, slot_item));
-            }
-
-            //add defaults if need
-            if (SpecSlotUsed < used)
-            {
-                var to_fill = used - SpecSlotUsed;
-                var defs_to_place = CustomSlotControler.GetDefaults(mech, location.mechLab.sim, sdef);
-
-                int n = 0;
-                while (to_fill > 0)
-                {
-                    while (to_fill < defs_to_place[n].si.SpecSlotUsed)
-                        n += 1;
-                    var def = defs_to_place[n];
-
-                    var slot = DefaultHelper.CreateSlot(def.item.ComponentDefID, def.item.ComponentDefType,
-                        location.mechLab);
-                    changes.Add(new AddDefaultChange(ChassisLocations.CenterTorso, slot));
-                        
-                    if (n + 1 < defs_to_place.Count)
-                        n += 1;
-                }
-            }
 
             return null;
         }
